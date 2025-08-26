0b4ea5a641f623cdad95e5074f2a96a8b1f87a5d
ChristiamR <christiancho0.9@gmail.com>
2025-08-25 22:44:40 -0500

    se agrego formulario para agregar nuevas imagenes, se cambio la clase container con form y se agrego id nuevos en las clases de los popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,20 @@
 let butEdit = document.querySelector(".profile__edit-btn");
-let popup = document.querySelector(".popup");
-let closeBtn = document.querySelector(".popup__button-close");
-
-let nameInput = document.querySelector(".popup__input-name");
-let jobInput = document.querySelector(".popup__input-about");
-
+let popupEdit = document.querySelector("#popup-edit");
+let closeBtn = popupEdit.querySelector(".popup__button-close");
+let nameInput = popupEdit.querySelector(".popup__input-name");
+let jobInput = popupEdit.querySelector(".popup__input-about");
 let profileName = document.querySelector(".profile__name");
 let profileRole = document.querySelector(".profile__role");
-
-let formElement = document.querySelector(".popup__container");
+let formEdit = document.querySelector(".popup__form");
 
 butEdit.addEventListener("click", () => {
   nameInput.value = "";
   jobInput.value = "";
-  popup.classList.add("popup_opened");
+  popupEdit.classList.add("popup_opened");
 });
 
 closeBtn.addEventListener("click", () => {
-  popup.classList.remove("popup_opened");
+  popupEdit.classList.remove("popup_opened");
 });
 
 function handleProfileFormSubmit(evt) {
@@ -26,7 +23,21 @@ function handleProfileFormSubmit(evt) {
   profileName.textContent = nameInput.value;
   profileRole.textContent = jobInput.value;
 
-  popup.classList.remove("popup_opened");
+  popupEdit.classList.remove("popup_opened");
 }
 
-formElement.addEventListener("submit", handleProfileFormSubmit);
+formEdit.addEventListener("submit", handleProfileFormSubmit);
+
+// --- Popup de Agregar Lugar ---
+let butAdd = document.querySelector(".profile__add-btn");
+let popupPlace = document.querySelector("#popup-place");
+let closeBtnPlace = popupPlace.querySelector(".popup__button-close");
+let formPlace = document.querySelector(".popup__form");
+
+butAdd.addEventListener("click", () => {
+  popupPlace.classList.add("popup_opened");
+});
+
+closeBtnPlace.addEventListener("click", () => {
+  popupPlace.classList.remove("popup_opened");
+});
