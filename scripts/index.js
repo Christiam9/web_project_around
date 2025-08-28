46fcac9b9848e71a24b2d186eb1620e597d9237f
ChristiamR <christiancho0.9@gmail.com>
2025-08-27 21:50:07 -0500

    se realizo varios bloques JS entre ellos galeria y se elimino de HTML creando una template, tambioen se creo funcion para agregar una tarjeta nueva por medio del formulario (popup)

--- scripts/index.js
+++ scripts/index.js
@@ -32,12 +32,86 @@ formEdit.addEventListener("submit", handleProfileFormSubmit);
 let butAdd = document.querySelector(".profile__add-btn");
 let popupPlace = document.querySelector("#popup-place");
 let closeBtnPlace = popupPlace.querySelector(".popup__button-close");
-let formPlace = document.querySelector(".popup__form");
+let formPlace = popupPlace.querySelector(".popup__form");
+let titleInput = formPlace.querySelector(".popup__input-title");
+let linkInput = formPlace.querySelector(".popup__input-link");
 
 butAdd.addEventListener("click", () => {
+  formPlace.reset();
   popupPlace.classList.add("popup_opened");
 });
 
 closeBtnPlace.addEventListener("click", () => {
   popupPlace.classList.remove("popup_opened");
+});
+// ---galeria de tarjetas---
+const templateCard = document.querySelector("#template-card");
+const sectionGallery = document.querySelector(".gallery__list");
+
+const initialCards = [
+  {
+    name: "isla de Galápagos",
+    link: "./images/islagalapagos.jpg",
+  },
+  {
+    name: "Amazonas",
+    link: "./images/Amazonia.jpg",
+  },
+  {
+    name: "Isla Clipperton",
+    link: "./images/isla_clipperton.jpg",
+  },
+  {
+    name: "Golfo de California",
+    link: "./images/golfo de california.jpg",
+  },
+  {
+    name: "Antartida",
+    link: "./images/antartida.jpg",
+  },
+  {
+    name: "Arrecifes de coral",
+    link: "./images/arrecifes de coral.jpg",
+  },
+];
+
+function createCard(title, link) {
+  const card = templateCard.content
+    .querySelector(".gallery__item")
+    .cloneNode(true);
+
+  const image = card.querySelector(".gallery__image");
+  const caption = card.querySelector(".gallery__title");
+
+  image.src = link;
+  image.alt = title;
+  caption.textContent = title;
+
+  return card;
+}
+
+initialCards.forEach((item) => {
+  const card = createCard(item.name, item.link);
+  sectionGallery.append(card);
+});
+
+// --- agregar tarjetas ---
+
+formPlace.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+
+  const title = titleInput.value;
+  const link = linkInput.value;
+
+  const newCard = createCard(title, link);
+
+  const firstCard = sectionGallery.querySelector(".gallery__item");
+  if (firstCard) {
+    sectionGallery.replaceChild(newCard, firstCard);
+  } else {
+    sectionGallery.append(newCard);
+  }
+
+  popupPlace.classList.remove("popup_opened");
+  formPlace.reset();
 });
